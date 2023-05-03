2933283f4d55c54c259283062f2122d733bf1069
Pawel Harasiuk <pharasiuk01@gmail.com>
2023-05-03 21:52:27 +0200

    Final 2

--- TPO5_HP_S24628/src/zad1/ChatClient.java
+++ TPO5_HP_S24628/src/zad1/ChatClient.java
@@ -1,13 +1,15 @@
 /**
- *
- *  @author Harasiuk Paweł S24628
- *
+ * @author Harasiuk Paweł S24628
  */
 
 package zad1;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.locks.*;
 
@@ -23,7 +25,7 @@ public class ChatClient extends Thread {
         this.id = id;
         this.host = host;
         this.port = port;
-        this.chatView = new StringBuilder(String.format("\n=== %s chat view\n", id));
+        this.chatView = new StringBuilder("\n=== " + id + " chat view\n");
     }
 
     public void login() {
@@ -37,23 +39,23 @@ public class ChatClient extends Thread {
                 if (n <= 0) throw new Exception("connection timeout");
             }
             this.request("login " + this.id);
-            this.chatView.append(String.format("%s logged in\n", id));
+            this.chatView.append(id).append(" logged in\n");
 
             this.start();
         } catch (Exception e) {
-            this.logException(e);
+            this.chatView.append("*** ").append(e);
         }
     }
 
     public void logout() {
         try {
             this.request("bye");
-            this.chatView.append(String.format("%s logged out", id));
+            this.chatView.append(id).append(" logged out");
 
             lock.lock();
             this.interrupt();
         } catch (Exception e) {
-            this.logException(e);
+            this.chatView.append("*** ").append(e);
         } finally {
             lock.unlock();
         }
@@ -61,23 +63,70 @@ public class ChatClient extends Thread {
 
     public void send(String request) {
         this.request(request);
-        this.chatView.append(id).append(": ").append(request).append("\n");
     }
 
     private void request(String message) {
         try {
             this.sc.write(StandardCharsets.UTF_8.encode(message + '@'));
         } catch (Exception e) {
-            this.logException(e);
+            this.chatView.append("*** ").append(e);
         }
     }
 
-    public String getChatView() {
-        return chatView.toString();
+    @Override
+    public void run() {
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+        int bytesRead;
+
+        while (!this.isInterrupted()) {
+            try {
+                lock.lock();
+                bytesRead = sc.read(buffer);
+                if (bytesRead > 0) {
+                    buffer.flip();
+                    Charset charset = StandardCharsets.UTF_8;
+                    CharBuffer cbuf = charset.decode(buffer);
+                    StringBuilder stringBuffer = new StringBuilder();
+                    boolean hasReadFullLine = false;
+
+                    while (cbuf.hasRemaining()) {
+                        char c = cbuf.get();
+
+                        if (c == '@') {
+                            hasReadFullLine = true;
+                            break;
+                        } else {
+                            stringBuffer.append(c);
+                        }
+                    }
+
+                    String response = stringBuffer.toString().trim();
+                    if (!(response.contains(id) && response.contains("logged"))) {
+                        if (response.trim().startsWith(id + ":")) {
+                            this.chatView.append(response.replace(":", "")).append("\n");
+                        } else {
+                            this.chatView.append(response).append("\n");
+                        }
+                    }
+
+
+                    if (hasReadFullLine) {
+                        break;
+                    }
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+
+            buffer.clear();
+        }
     }
 
-    private void logException(Exception exception) {
-        this.chatView.append(String.format("*** %s", exception.toString()));
+
+    public String getChatView() {
+        return chatView.toString();
     }
 
     public String getClientId() {
